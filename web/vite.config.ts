3e288fc2949e3124767bbbcdebc8595008088c24
David Hagerty <david@dathagerty.com>
2026-02-11 12:47:11 -0500

    chore(web): add Vite, Svelte, and TypeScript config files

---
+++ web/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import svelte from '@sveltejs/vite-plugin-svelte';
+
+export default defineConfig({
+  plugins: [svelte()],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:7400',
+        changeOrigin: true,
+      },
+      '/ws': {
+        target: 'ws://localhost:7400',
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
+});
